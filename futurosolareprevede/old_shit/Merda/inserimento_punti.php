7988a5cd0bbceb3b3cb3be5dd3bcd8998a82a750
Luca Fiorini <luca.fio45@gmail.com>
2023-09-25 16:41:43 +0200

    Fixed datapoints

--- futurosolareprevede/old_shit/Merda/inserimento_punti.php
+++ futurosolareprevede/old_shit/Merda/inserimento_punti.php
@@ -1,8 +1,8 @@
 <?php
-include("../Utility/functions.php");
+include("../../Utilities/functions.php");
 
 //var_dump($conn);
-$conn = getSqlConnection();
+$conn = getDbConnection();
 $gpx = simplexml_load_file("../data/futurosolareprevedewaypoints.gpx");
 $path = [];
 $i = 0;
@@ -10,17 +10,17 @@ $i = 0;
 foreach ($gpx->wpt as $pt) {
   $path[$i]['lat'] = (float) $pt->attributes()['lat'];
   $path[$i]['lon'] = (float) $pt->attributes()['lon'];
-  $path[$i]['distance_to_next'] = 5; ///table/v1/{profile}/{coordinates}?{sources}=[{elem}...];&{destinations}=[{elem}...]&annotations={duration|distance|duration,distance}
+  $path[$i]['distance_to_next'] = 5; 
 	$path[$i]['ele'] = -1;
   $i++;
 }
 
 echo "<pre>";
 echo $i;
-for($j= count($path) -1 ; $j >= 0; $j-- ){
+for($j=0 ; $j < sizeof($path); $j++ ){
 
-  $sql = "INSERT INTO points (point_ID, latest_weather_data, distance_to_next, latitude, longitude, elevation)
-    VALUES (". (count($path) - $j - 1) .", '{}', ".$path[$j]['distance_to_next'].",".$path[$j]['lat'].','.$path[$j]['lon'].', '.$path[$j]['ele'].')';
+  $sql = "INSERT INTO points (point_ID, latitude, longitude, elevation)
+    VALUES (".strval($j).",".$path[$j]['lat'].','.$path[$j]['lon'].', '.$path[$j]['ele'].')';
 
   if(false === mysqli_query($conn, $sql)) {
       exit("Errore: impossibile eseguire la query. " . mysqli_error($conn) . '\n' . $sql);
